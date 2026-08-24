4b948b6ac813e7e26fc4a3f36a4713a012313da0
shr0ukgomaa33 <shroukgomaa8787@gmail.com>
2026-08-25 01:32:14 +0300

    Q1

--- Assignment oop5/Program.cs
+++ Assignment oop5/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Assignment_oop5
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_oop5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1
+
+            // a) When you assign one object variable to another, both variables refer to the same object in memory.
+
+            // b) No. Assigning one object variable to another does not create a new object. It only copies the reference to the existing object.
+
+            // c) Copying the reference: Both variables point to the same object, so changes through one variable can affect the other.
+            //       Copying the object: A new, separate object is created with its own data
+
+            #endregion
+
+
+
+
+
             
+
+
+
+
+
+
+
+        
+
         }
     }
 }
